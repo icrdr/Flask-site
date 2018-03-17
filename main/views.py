f711671279459bb9004bb7de0c3aa809f97f9c99
icrdr <icrdr2010@gmail.com>
2018-03-18 02:25:30 +0800

    test

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from main import app
 
+users = [
+	{'nickname':'sss', 'name':'xxx'},
+	{'nickname':'web', 'name':'fev'},
+]
+
 @app.route("/")
 @app.route("/index")
-@app.route("/threejs")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html",
+	user = users[1],
+	title = 'welcome')
