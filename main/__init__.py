63a7900860d8973caa22fb4a401b4e25ae39412c
icrdr <icrdr2010@gmail.com>
2018-03-20 23:56:08 +0800

    而我热

--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-
+from flaskext.mysql import MySQL
 from . import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 CSRFProtect(app)
+
+mysql = MySQL()
+mysql.init_app(app)
 
 from . import views
--- main/views.py
+++ main/views.py
@@ -1,23 +1,41 @@
-from flask import render_template, redirect
+from flask import render_template, request, jsonify, flash, redirect, url_for
 from . import app
 
 #forms
 from .forms import *
 
 #index
-@app.route("/", methods=('GET', 'POST'))
-@app.route("/index", methods=('GET', 'POST'))
-def index():
-	title = "icrdr"
-	return render_template("index.html",
-	title = title)
+@app.route("/")
+@app.route("/index")
+def homepage():
+	cursor = mysql.
+	return render_template("index.html")
 
 #experiment lung segment display
 @app.route("/experiment/lungSeg")
-def lungSeg():
+def lungSeg_page():
     return render_template("lungSeg.html")
-	
+
 #404
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(e):
     return render_template('404.html'), 404
+	
+@app.route('/process', methods=['POST'])
+def process():
+    a = int(request.form['input_a'])
+    b = int(request.form['input_b'])
+    return jsonify({'output': a+b })
+
+ #forms test
+@app.route("/forms", methods=['GET', 'POST'])
+def forms_page():
+	error = None
+	if request.method == "POST":
+		attempted_info = request.form['info']
+		flash(attempted_info)
+		if attempted_info == "fuck":
+			return redirect(url_for('homepage'))
+		else:
+			error = "not a fuck. please fuck."
+	return render_template("forms.html", error = error)
